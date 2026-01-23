676007b49b73ea7985d531e33c10dae7b35aac45
JaydenAngulo <jaydenangulo21@icloud.com>
2026-01-23 00:23:44 +0000

    first

--- codequest.py
+++ codequest.py
@@ -1,3 +1,14 @@
 # Problem 1:
 
-strings = int(input())
+def letters_after_vowels(text):
+    vowels = "aeiou"
+    result = ""
+
+    for i in range(len(text)-1):
+        if text[i] in vowels:
+            result += text[i + 1]
+    return result
+
+user_input = input("Enter a string: ")
+output = letters_after_vowels(user_input)
+print(output)
