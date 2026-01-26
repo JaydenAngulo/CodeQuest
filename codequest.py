7949ab10b15795888a5fe0a834967bbee26e49cb
JaydenAngulo <jaydenangulo21@icloud.com>
2026-01-26 17:24:03 +0000

    last time

--- codequest.py
+++ codequest.py
@@ -1,4 +1,4 @@
-# Problem 1:
+# Problem 1-5 points:
 
 # n = int(input())
 # list = []
@@ -24,7 +24,7 @@
 # for _ in range(n):
 #     print(next(answer_iterator))
 
-# Problem 2:
+# Problem 2-5 points:
 
 # n = int(input())
 # list = []
@@ -40,20 +40,64 @@
 # for _ in range(n):
 #     print(next(answer_iterator))
 
-# Problem 3:
+# Problem 3-25 points:
 
-cases = int(input())
-missing = []
+# cases = int(input())
+# missing = []
 
-for _ in range(cases):
-    N = int(input())
-    numbers = [int(s) for s in input().split()]
-    answers = list(range(1, N + 1))
-    for i in answers:
-        if i not in numbers:
-            missing.append(int(i))
+# for _ in range(cases):
+#     N = int(input())
+#     numbers = [int(s) for s in input().split()]
+#     answers = list(range(1, N + 1))
+#     for i in answers:
+#         if i not in numbers:
+#             missing.append(int(i))
 
-answer_iterator = iter(missing)
+# answer_iterator = iter(missing)
 
-for _ in range(cases):
-    print(next(answer_iterator))
+# for _ in range(cases):
+#     print(next(answer_iterator))
+
+# Problem 4-50 points:
+
+n = int(input())
+
+morse_to_letter = {}
+letter_to_morse = {}
+
+for _ in range(26):
+    code = input().split(maxsplit=1)
+    letter = code[0]
+    morse = code[1]
+    letter_to_morse[letter] = morse
+    morse_to_letter[morse] = letter
+
+# phrase = input()
+# encoded_phrase = []
+
+# for char in phrase:
+#     if char == " ":
+#         encoded_phrase.append("      ")
+#     else:
+#         encoded_phrase.append(letter_to_morse[char])
+
+# print('   '.join(encoded_phrase))
+
+coded_message = input()
+print(coded_message)
+decoded_phrase = []
+coded_letter = []
+
+for i, ch in enumerate(coded_message):
+    if ch == " " and coded_message[i+1] == " " or ch == " " and coded_message[i-1] == " ":
+        continue
+    else:
+        coded_letter.append(ch)
+    if coded_message[i+1] and coded_message[i+2] and coded_message[i+3] == " " or i == len(coded_message) - 1:
+        coded_letter.append(ch)
+        morse_letter = " ".join(coded_letter)
+        print(morse_letter)
+        decoded_phrase.append(morse_to_letter[morse_letter])
+        coded_letter.clear()
+
+print(decoded_phrase)
